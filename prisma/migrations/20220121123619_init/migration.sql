96d430c02bc9fa644e6dfc04d281c8d84efe79db
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-01-21 09:38:34 -0300

    setup prisma

---
+++ prisma/migrations/20220121123619_init/migration.sql
@@ -0,0 +1,65 @@
+-- CreateTable
+CREATE TABLE "orientador" (
+    "siape" INTEGER NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+
+    CONSTRAINT "orientador_pkey" PRIMARY KEY ("siape")
+);
+
+-- CreateTable
+CREATE TABLE "aluno" (
+    "matricula" INTEGER NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+
+    CONSTRAINT "aluno_pkey" PRIMARY KEY ("matricula")
+);
+
+-- CreateTable
+CREATE TABLE "trabalho" (
+    "id" SERIAL NOT NULL,
+    "titulo" TEXT NOT NULL,
+    "mat_aluno" INTEGER NOT NULL,
+
+    CONSTRAINT "trabalho_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "versao" (
+    "id" SERIAL NOT NULL,
+    "id_trabalho" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "versao_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sugestao" (
+    "id" SERIAL NOT NULL,
+    "valor" VARCHAR(500) NOT NULL,
+    "siape_orientador" INTEGER NOT NULL,
+    "id_versao" INTEGER NOT NULL,
+
+    CONSTRAINT "sugestao_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orientador_email_key" ON "orientador"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "aluno_email_key" ON "aluno"("email");
+
+-- AddForeignKey
+ALTER TABLE "trabalho" ADD CONSTRAINT "trabalho_mat_aluno_fkey" FOREIGN KEY ("mat_aluno") REFERENCES "aluno"("matricula") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "versao" ADD CONSTRAINT "versao_id_trabalho_fkey" FOREIGN KEY ("id_trabalho") REFERENCES "trabalho"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sugestao" ADD CONSTRAINT "sugestao_siape_orientador_fkey" FOREIGN KEY ("siape_orientador") REFERENCES "orientador"("siape") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sugestao" ADD CONSTRAINT "sugestao_id_versao_fkey" FOREIGN KEY ("id_versao") REFERENCES "versao"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
