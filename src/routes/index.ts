0f5653a7bf2c58b11cea406b00935d30051d2569
JoaoAlvesC <jooao.v@outlook.com>
2022-01-25 13:38:01 -0300

    create use of version and suggestion routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { advisorRoutes } from "./_advisor";
 import { projectRoutes } from "./_project";
 import { studentRoutes } from "./_student";
+import { versionRoutes } from "./_version";
+import { suggestionRoutes } from "./_suggestion";
 
 export const routes = {
 	advisor: advisorRoutes,
 	project: projectRoutes,
 	student: studentRoutes,
+	version: versionRoutes,
+	suggestion: suggestionRoutes,
 };
