1dfce7428bae94e1adc6687607506b264b12cd1c
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-02-15 20:19:50 -0300

    add cors handler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { routes } from "routes";
+import cors from "cors";
 
 const api = express();
 api.use(express.json());
+api.use(cors());
 
 api.use("/orientador", routes.advisor);
 api.use("/trabalho", routes.project);
