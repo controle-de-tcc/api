81a42b930b19d5404b386a7c4b8696623c1ab597
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-02-27 18:10:25 -0300

    remove unused

--- src/controllers/_baseOperations.ts
+++
@@ -1,5 +0,0 @@
-export abstract class BaseOperations<Type> {
-	abstract create(body: Type | any): Promise<Type>;
-	abstract list(): Promise<Array<Type | any>>;
-	abstract get?(email: string): Promise<Type | null>;
-}
